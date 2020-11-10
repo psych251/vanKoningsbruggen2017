124eb48b4b5278c39c60723c6097d49862ac3dc8
ayl888 <angela8@stanford.edu>
2020-11-10 00:32:05 -0800

    Update amp_script_qualtrics.js
    updated with base_url to newimages only folder

--- amp_script_qualtrics.js
+++ amp_script_qualtrics.js
@@ -43,11 +43,30 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 		{
 			nameForLogging : 'examplePrime', //Will be used in the logging
 			//An array of all media objects for this category.
-			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+			mediaArray : [{image : 'prac1.jpg'}, {image : 'prac2.jpg'}, {image : 'prac3.jpg'}]
 		},
 
 		base_url : {//Where are your images at?
-			image : 'https://psych251.github.io/vanKoningsbruggen2017/inquisit_stimuli'
-		}
+			image : 'https://psych251.github.io/vanKoningsbruggen2017/inquisit_stimuli_newimagesonly'
+		}, 
+		
+		//Changing params to make task the same as original paper
+		
+		//(Changed trial # in block & example to match vanKoningsbruggen2017)
+		trialsInBlock : [60], //Changed number of trials to 1 block of 60
+		trialsInExample : [3], //Change to 3 for example practice block 
+
+		//Duration parameters (updated from MinnoJS script to match vanKoningsbruggen2017)
+		fixationDuration : -1, //It means that by default we do not use fixation.
+		primeDuration : 75, 
+		postPrimeDuration : 125, //Duration of blank screen between prime and target.
+		targetDuration : 100, //Duration of target presentation.
+		ITI : 250, //Duration between trials.
+		
+		//For the example block (often practice) (updated from MinnoJS script to match vanKoningsbruggen2017)
+		exampleBlock_fixationDuration : -1, 
+		exampleBlock_primeDuration : 75, 
+		exampleBlock_postPrimeDuration : 125, 
+		exampleBlock_targetDuration : 100, 
 	});
 });
